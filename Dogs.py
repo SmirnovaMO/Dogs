2c72b79f77c6287747cb1deba29a779d91d9927c
892639o8773-lgtm <892639o8773@gmail.com>
2025-08-27 20:50:56 +0300

    Создано главное окно и метка и кнопка.

---
+++ Dogs.py
@@ -0,0 +1,16 @@
+from tkinter import *
+import requests
+from PYL import Image, ImageTk
+from IO import BytesIO
+
+window = Tk()
+window.title('Картинки с собачками')
+window.geometry('360x420')
+
+label = Label()
+label.pack(pady=10)
+
+button = Button(text='Загрузить изображение', command=show_image)
+button.pack(pady=10)
+
+window.mainloop()
